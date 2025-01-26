8224a9d331b2bed83f66f75b03d5d7fb0ddb6ff4
Vyshnav GR <vyshnav.gr@ivorytechnologies.com>
2025-01-26 21:03:54 +0530

    fff

--- basic maths/digits.cpp
+++ basic maths/digits.cpp
@@ -1,14 +1,32 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     int count=0 ;
+//     cout <<"enter the digit";
+//     cin >> n;
+//     while(n>0){
+//         int lastDigit=n%10;
+//        cout << "Last digit is: " << lastDigit << endl;
+//         count++;
+//         n=n/10;
+//     }
+//     cout<<"total number is "<<count;
+//     return 0;
+// }
+
+#include <iostream>
+#include <cmath> 
 using namespace std;
-int main(){
+
+int main() {
     int n;
-    cout <<"enter the digit";
+    cout << "Enter a number: ";
     cin >> n;
-    while(n>0){
-        int lastDigit=n%10;
-       cout << "Last digit is: " << lastDigit << endl;
-        n=n/10;
-    }
+
+
+    int cnt = (int)(log10(n) + 1);
+    cout << "The number of digits is: " << cnt << endl;
+
     return 0;
 }
-
