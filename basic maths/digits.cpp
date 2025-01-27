71419c0fceec25bdaa08c96e2c60fda73a84fa27
Vyshnav GR <vyshnav.gr@ivorytechnologies.com>
2025-01-27 22:12:01 +0530

    27/01/2025

--- basic maths/digits.cpp
+++ basic maths/digits.cpp
@@ -109,3 +109,29 @@
 //    }
 // }
 
+
+
+
+
+//find all the divisers 
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    vector<int> v;
+    cin>>n;
+    for(int i=1;i*i<=n;i++){
+    if(n%i==0){
+    v.push_back(i);
+    if(n/i!=i){
+        v.push_back(n/i);
+    }
+    }
+    }
+
+    sort(v.begin(),v.end());
+
+    for(auto it:v)cout <<it <<" ";
+}
+
