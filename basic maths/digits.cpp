975a7d9e5148d1264d7669e339d2c87ebd6de718
Vyshnav GR <vyshnav.gr@ivorytechnologies.com>
2025-01-27 21:24:44 +0530

    27/01/2025

--- basic maths/digits.cpp
+++ basic maths/digits.cpp
@@ -1,32 +1,111 @@
+// extract number
+
 // #include<iostream>
 // using namespace std;
 // int main(){
 //     int n;
-//     int count=0 ;
-//     cout <<"enter the digit";
+//      cout <<"enter the digit";
 //     cin >> n;
 //     while(n>0){
 //         int lastDigit=n%10;
 //        cout << "Last digit is: " << lastDigit << endl;
-//         count++;
-//         n=n/10;
+//        n=n/10;
 //     }
-//     cout<<"total number is "<<count;
+
 //     return 0;
 // }
 
-#include <iostream>
-#include <cmath> 
-using namespace std;
-
-int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
 
 
-    int cnt = (int)(log10(n) + 1);
-    cout << "The number of digits is: " << cnt << endl;
+//count of digits
 
-    return 0;
-}
+// #include <iostream>
+// #include <cmath> 
+// using namespace std;
+
+// int main() {
+//     int n;
+//     cout << "Enter a number: ";
+//     cin >> n;
+
+
+//     int cnt = (int)(log10(n) + 1);
+//     cout << "The number of digits is: " << cnt << endl;
+
+//     return 0;
+// }
+
+
+
+
+//reverse of number ////////////
+
+// #include <iostream>
+// #include<cmath>
+// using namespace std;
+// int main(){
+// int n;
+// cout <<"enter the digits";
+// cin >>n;
+// int rev=0;
+// while(n>0){
+// int lastDigit=n%10; 
+//  rev=(rev*10)+lastDigit;
+// n=n/10;
+// }
+
+// cout<<rev;
+
+// }
+
+
+//palindrome
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// cout <<"enter a number";
+// cin>>n;
+// int dup=n;
+// int rev=0;
+// while(n>0){
+//     int ld=n%10;
+//     rev=(rev*10)+ld;
+//     n=n/10;
+// }
+// if(dup==rev){
+//    cout <<"palindrome";
+// }else{
+//      cout <<"not a palindrome";
+// }
+//     return 0;
+// }
+
+
+
+//amstrong
+
+// #include <iostream>
+// #include <cmath>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout <<"enter a number";
+//     cin>>n;
+//     int dup=n;
+//     int ams=0;
+//     int count=(int)(log10(n)+1);
+//     while(n>0){
+//     int ld=n%10;
+//      ams += pow(ld, count);
+//     n=n/10;
+//    }
+//    if(ams==dup){
+//     cout<<"amstrong";
+//    }else{
+//     cout<<"false";
+//    }
+// }
+
